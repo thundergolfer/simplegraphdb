142c4c91e7a30d31b743b0afb76d292b9cd38df0
thundergolfer <jonathon.bel.melbourne@gmail.com>
2017-11-22 12:20:18 +1100

    adding scripts to create a JSON db of twitter follow relationships

---
+++ scripts/create_twitter_followers_graph_example_db.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "You will need to create a Twitter App to get consumer keys."
+echo "Redirecting you to app creation webpage. Make sure you're logged in to Twitter"
+echo "Important: keep your new application open in the browser. You'll be asked by this script for the keys."
+sleep 1
+
+$(xdg-open "https://apps.twitter.com/app/new")
+
+read -p "Enter 'Consumer Key' (from 'Keys and Access Tokens' tab): " CONSUMER_KEY
+read -p "Enter 'Consumer Secret': " CONSUMER_SECRET
+
+echo "Now, you haven't yet authorised this app for your account. Go down to the bottom of the tab you're on and "
+echo "click the button to create an access token"
+
+sleep 3
+
+echo "After clicking, you should see 'Access Token' and 'Access Token Secret' appear"
+read -p "Enter 'Access Token': " ACCESS_TOKEN
+read -p "Enter 'Access Token Secret': " ACCESS_TOKEN_SECRET
+
+echo "Great! One last thing, we need your twitter handle. It starts with '@'"
+read -p "Enter Twitter handle: " TWITTER_HANDLE
+
+KEY_FILE_CONTENTS="{\"consumer_key\": \"$CONSUMER_KEY\", \"consumer_secret\": \"$CONSUMER_SECRET\", \"access_token\": \"$ACCESS_TOKEN\", \"access_token_secret\": \"$ACCESS_TOKEN_SECRET\", \"handle\": \"$TWITTER_HANDLE\"}"
+echo "****************************************************"
+echo "Creating .gitignore-d private credentials .json file"
+echo "****************************************************"
+echo "$KEY_FILE_CONTENTS" > private_twitter_credentials.json
---
+++ scripts/scrape_twitter_followers.py
@@ -0,0 +1,60 @@
+import json
+import tweepy
+import os
+import time
+
+this_files_path = os.path.dirname(os.path.realpath(__file__))
+private_twitter_creds_path = "private_twitter_credentials.json"
+
+
+def process_group(subject, prop, group):
+    for user in group:
+        triple = {"subject": subject, "prop": prop, "object": user}
+        db["triples"].append(triple)
+        print("Processed: {}".format(triple))
+
+
+with open(os.path.join(this_files_path, private_twitter_creds_path), 'r') as f:
+    keys = json.load(f)
+
+print(keys)
+
+consumer_key = keys["consumer_key"]
+consumer_secret = keys["consumer_secret"]
+access_token = keys["access_token"]
+access_token_secret = keys["access_token_secret"]
+twitter_handle = keys["handle"] if keys["handle"][0] != '@' else keys["handle"][1:]
+
+auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+if api.verify_credentials:
+    print("Successfully authenticated!")
+else:
+    print("There was a mistake made when receiving credentials. Remove 'private_twitter_credentials.json' and try the script again")
+
+#us = tweepy.Cursor(api.followers, screen_name=twitter_handle).items()
+db = {"triples": []}
+
+your_friends = tweepy.Cursor(api.friends, screen_name=twitter_handle).items()
+process_group(twitter_handle, "follows", [user.screen_name for user in your_friends])
+
+who_follows_you = tweepy.Cursor(api.followers, screen_name=twitter_handle).items()
+process_group(twitter_handle, "is_followed_by", [user.screen_name for user in who_follows_you])
+
+
+for user in your_friends:
+    import pdb
+    pdb.set_trace()
+    their_friends = tweepy.Cursor(api.friends_ids, screen_name=user.screen_name).items()
+    process_group(user.screen_name, "follows", [u.screen_name for u in api.lookup_users(user_ids=their_friends)])
+    who_follows_them = tweepy.Cursor(api.followers_ids, screen_name=user.screen_name).items()
+    process_group(user.screen_name, "is_followed_by", [u.screen_name for u in api.lookup_users(user_ids=who_follows_them)])
+
+output_filepath = 'your_twitter_example_db.json'
+print("Writing file to {}".format(output_filepath))
+with open(output_filepath, 'w') as f:
+    json.dump(db, f)
+
+print("done!")
