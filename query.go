f72de6c493ffa3e683fa61692d7be2e7049aff60
Jonathon Belotti <jbelotti@atlassian.com>
2017-12-03 12:54:29 +1100

    move result presentation functionality into own file

--- query.go
+++ query.go
@@ -21,34 +21,7 @@ func RunQuery(query string, hexastore *Hexastore) string {
 	mappedResults := mapTriplePartsToVars(hexastore, queryModel, rawResults)
 	resultsGrid := buildResultsGrid(returnVars, mappedResults, len(*rawResults))
 
-	return presentResultGrid(resultsGrid)
-}
-
-func presentResultGrid(resultsGrid *[][]string) (presentable string) {
-	max_cell_size := 30
-	presentable = ""
-	for i, row := range *resultsGrid {
-		for _, col := range row {
-			curr_len := len(col)
-			if curr_len >= max_cell_size {
-				presentable += col[:max_cell_size-2] + ".." + " | "
-			} else {
-				presentable += rightPad2Len(col, " ", max_cell_size) + " | "
-			}
-		}
-		presentable += "\n"
-		if i == 0 {
-			presentable += strings.Repeat("-", (max_cell_size+2)*len(row)) + "\n"
-		}
-	}
-	return
-}
-
-func rightPad2Len(s string, padStr string, overallLen int) string {
-	var padCountInt int
-	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
-	var retStr = s + strings.Repeat(padStr, padCountInt)
-	return retStr[:overallLen]
+	return PresentResultGrid(resultsGrid)
 }
 
 func buildResultsGrid(returnVars []string, mappedResults map[string][]string, numResults int) *[][]string {
