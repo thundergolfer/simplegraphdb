2d5e5aef1dcce7f9154401140747fbe1454f3b32
Jonathon Belotti <jbelotti@atlassian.com>
2017-12-03 12:35:00 +1100

    improve output formatting using truncation where necessary

--- query.go
+++ query.go
@@ -25,17 +25,30 @@ func RunQuery(query string, hexastore *Hexastore) string {
 }
 
 func presentResultGrid(resultsGrid *[][]string) (presentable string) {
+	max_cell_size := 30
 	presentable = ""
 	for i, row := range *resultsGrid {
 		for _, col := range row {
-			presentable += (col + " | ")
+			curr_len := len(col)
+			if curr_len >= max_cell_size {
+				presentable += col[:max_cell_size-2] + ".." + " | "
+			} else {
+				presentable += rightPad2Len(col, " ", max_cell_size) + " | "
+			}
 		}
 		presentable += "\n"
 		if i == 0 {
-			presentable += "------------------------------------------------\n"
+			presentable += strings.Repeat("-", (max_cell_size+2)*len(row)) + "\n"
 		}
 	}
 	return
+}
+
+func rightPad2Len(s string, padStr string, overallLen int) string {
+	var padCountInt int
+	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
+	var retStr = s + strings.Repeat(padStr, padCountInt)
+	return retStr[:overallLen]
 }
 
 func buildResultsGrid(returnVars []string, mappedResults map[string][]string, numResults int) *[][]string {
