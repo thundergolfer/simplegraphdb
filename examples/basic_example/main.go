6110e5d15349faba3ab7feae326108900c10159d
Jonathon Belotti <jbelotti@atlassian.com>
2017-12-23 16:32:43 +1100

    improve basic example UX

--- examples/basic_example/main.go
+++ examples/basic_example/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/thundergolfer/simplegraphdb"
 )
@@ -9,10 +11,29 @@ import (
 func main() {
 	fmt.Println("vim-go")
 	var result string
-	query := "SELECT ?x WHERE { ?x 'likes' 'money' }"
+	store, err := simplegraphdb.InitTestHexastore()
+	if err != nil {
+		fmt.Println("Error: failed to initialise example store")
+		os.Exit(1)
+	}
+	exampleQuery := "SELECT ?x WHERE { ?x 'likes' 'money' }"
 
-	fmt.Println("Trying to do runQuery()")
-	store := simplegraphdb.InitTestHexastore()
-	result = simplegraphdb.RunQuery(query, store)
+	fmt.Println("Example query: ")
+	fmt.Println(exampleQuery)
+
+	fmt.Println("Result of example query: ")
+	result = simplegraphdb.RunQuery(exampleQuery, store)
 	fmt.Println(result)
+
+	fmt.Println("END DEMO\n---------------------------------")
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter simpleSPARQL style query: ")
+		query, _ := reader.ReadString('\n')
+		results := simplegraphdb.RunQuery(query, store)
+		fmt.Println("Result: ")
+		fmt.Println("-------------------------------------------------------------------------")
+		fmt.Println(results)
+	}
 }
